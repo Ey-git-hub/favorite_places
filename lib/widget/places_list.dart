94e4cb8f7da1fd21af0a3f64f3a65af2038c3bff
Ey-git-hub <eyob.tadesse@astu.edu.et>
2026-08-16 15:32:23 +0300

    pushed the add placeses screen

--- lib/widget/places_list.dart
+++ lib/widget/places_list.dart
@@ -1,4 +1,4 @@
-import 'package:favorite_places/main.dart';
+// import 'package:favorite_places/main.dart';
 import 'package:favorite_places/model/Places.dart';
 import 'package:flutter/material.dart';
 
@@ -8,7 +8,7 @@ class PlacesList extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     if(places.isEmpty){
-      return Center(child: Text("No places added", style: Theme.of(context).textTheme.titleMedium!.copyWith(
+      return Center(child: Text("No places added", style: Theme.of(context).textTheme.bodyLarge!.copyWith(
         color:Theme.of(context).colorScheme.onBackground),),);
     }
     return ListView.builder(itemCount: places.length ,itemBuilder: (context, index) => ListTile(
