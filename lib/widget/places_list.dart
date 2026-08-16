b4c49472247e65b656f11d10c110d684e49d3e87
Ey-git-hub <eyob.tadesse@astu.edu.et>
2026-08-16 16:00:57 +0300

    update on places list

--- lib/widget/places_list.dart
+++ lib/widget/places_list.dart
@@ -3,7 +3,7 @@ import 'package:favorite_places/model/Places.dart';
 import 'package:flutter/material.dart';
 
 class PlacesList extends StatelessWidget{
-  PlacesList({super.key,required this.places});
+  const PlacesList({super.key,required this.places});
   final List<Places> places;
   @override
   Widget build(BuildContext context) {
