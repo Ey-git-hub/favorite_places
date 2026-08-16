dfaa05c77b1a8e5e903bc1937a529c7fd8050a3e
Ey-git-hub <eyob.tadesse@astu.edu.et>
2026-08-16 16:40:35 +0300

    added the riverpod watch method to watch the changes in the userplace provider

--- lib/screens/places.dart
+++ lib/screens/places.dart
@@ -1,11 +1,14 @@
+import 'package:favorite_places/providers/user_places.dart';
 import 'package:favorite_places/screens/add_places.dart';
 import 'package:favorite_places/widget/places_list.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class PlacesListScreen extends StatelessWidget{
+class PlacesListScreen extends ConsumerWidget{
   const PlacesListScreen({super.key});
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context,WidgetRef ref) {
+    final userPlaces=ref.watch(userPlaceProvider);
     return Scaffold(
       appBar: AppBar(
         title: Text("Your Places"),
@@ -22,7 +25,7 @@ class PlacesListScreen extends StatelessWidget{
           )
         ],
       ),
-      body: PlacesList(places: []) ,
+      body: PlacesList(places: userPlaces) ,
     )
   ;
   }
