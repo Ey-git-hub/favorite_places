bdb12580c08836a68ca5c52dd0dfcf6b038ef520
Ey-git-hub <eyob.tadesse@astu.edu.et>
2026-08-16 15:31:12 +0300

    added the implementation for add_places

---
+++ lib/screens/add_places.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class AddPlacesScreen extends StatefulWidget{
+  const AddPlacesScreen({super.key});
+  @override
+  State<AddPlacesScreen> createState() {
+    return _AddPlacesScreen();
+  }
+}
+
+class _AddPlacesScreen extends State<AddPlacesScreen>{
+  final _titleController=TextEditingController();
+
+  @override 
+  void dispose(){
+    _titleController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("ADD NEW PLACES"),
+      ),
+      body:SingleChildScrollView(
+      padding: EdgeInsets.all(12)
+      ,child: Column(
+        children: [
+          TextField(
+            decoration: InputDecoration(labelText: "Title",
+            ),
+            controller:_titleController ,
+            style: TextStyle(color:Theme.of(context).colorScheme.onBackground,
+          )),
+          SizedBox(height: 16),
+          ElevatedButton.icon(onPressed: (){}, 
+          icon:Icon(Icons.add), 
+          label: Text("ADD PLACE"))
+        ],
+        
+      ),
+
+      ),
+
+    );
+  }
+}
