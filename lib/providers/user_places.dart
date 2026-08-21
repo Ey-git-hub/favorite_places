4277edc02ad4dc24ce33bad369b58b9992059d72
Ey-git-hub <eyob.tadesse@astu.edu.et>
2026-08-21 13:41:11 +0300

    update the provider using the image property

--- lib/providers/user_places.dart
+++ lib/providers/user_places.dart
@@ -1,11 +1,11 @@
 import 'package:favorite_places/model/Places.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
+import 'dart:io';
 class UserPlaceNotifier extends StateNotifier<List<Places>>{
 UserPlaceNotifier() :super([]);
 
-void addPlace(String title){
-  final newPlace=Places(title: title);
+void addPlace(String title, File image){
+  final newPlace=Places(title: title, image: image);
   state=[newPlace,...state];
 }
 }
