3a0cfda15e13db1972d7fb9579ab8fce288daefd
Ey-git-hub <eyob.tadesse@astu.edu.et>
2026-08-21 13:44:40 +0300

    added the feature that to display the images a taken and to see details after tapping on the added places

--- lib/widget/places_list.dart
+++ lib/widget/places_list.dart
@@ -1,5 +1,6 @@
 // import 'package:favorite_places/main.dart';
 import 'package:favorite_places/model/Places.dart';
+import 'package:favorite_places/screens/places_detail.dart';
 import 'package:flutter/material.dart';
 
 class PlacesList extends StatelessWidget{
@@ -12,10 +13,21 @@ class PlacesList extends StatelessWidget{
         color:Theme.of(context).colorScheme.onSurface),),);
     }
     return ListView.builder(itemCount: places.length ,itemBuilder: (context, index) => ListTile(
+      leading:CircleAvatar(
+        radius: 26,
+        backgroundImage: FileImage(places[index].image),
+      ),
       title: Text(places[index].title,
       style: Theme.of(context).textTheme.titleMedium!.copyWith(
         color:Theme.of(context).colorScheme.onSurface),
-        )
+        ),
+        onTap: (){
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (context)=>PlacesDetailScreen(place: places[index],)
+            )
+          );
+        },
     ),
     );
   }
