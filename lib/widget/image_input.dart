6cc10d7a5c70b18f5555c1d2bca2a0ee6a9ae14e
Ey-git-hub <eyob.tadesse@astu.edu.et>
2026-08-21 11:08:59 +0300

    added the import for the image picker

--- lib/widget/image_input.dart
+++ lib/widget/image_input.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ImageInput extends StatefulWidget {
   const ImageInput({super.key});
@@ -7,14 +8,20 @@ class ImageInput extends StatefulWidget {
   State<ImageInput> createState() => _ImageInputState();
 }
 class _ImageInputState extends State<ImageInput> {
+  void _takePicture() {
+    
+  }
   @override
   Widget build(BuildContext context) {
     return Container(
       height: 200,
       width: double.infinity,
+      decoration: BoxDecoration(
+        border: Border.all(width: 1, color: Colors.grey),
+      ),
       alignment: Alignment.center,
       child: TextButton.icon(
-        onPressed: (){},
+        onPressed: _takePicture,
         icon: Icon(Icons.camera),
         label: Text("Take Picture"),
       ),
