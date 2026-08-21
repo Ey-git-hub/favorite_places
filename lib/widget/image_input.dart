a1cf640d2cdcca1c6c83bbefcd2627400a37d1dc
Ey-git-hub <eyob.tadesse@astu.edu.et>
2026-08-21 10:54:41 +0300

    added  the image input widget for add places screen

---
+++ lib/widget/image_input.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class ImageInput extends StatefulWidget {
+  const ImageInput({super.key});
+  
+  @override
+  State<ImageInput> createState() => _ImageInputState();
+}
+class _ImageInputState extends State<ImageInput> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 200,
+      width: double.infinity,
+      alignment: Alignment.center,
+      child: TextButton.icon(
+        onPressed: (){},
+        icon: Icon(Icons.camera),
+        label: Text("Take Picture"),
+      ),
+    );
+  }
+}
