3f3707ec313b2a4ebfa608cbbcc51ff4c4855a14
Ey-git-hub <eyob.tadesse@astu.edu.et>
2026-08-21 13:43:05 +0300

    added padding on the places screen

--- lib/screens/places.dart
+++ lib/screens/places.dart
@@ -25,7 +25,8 @@ class PlacesListScreen extends ConsumerWidget{
           )
         ],
       ),
-      body: PlacesList(places: userPlaces) ,
+      body: Padding(padding: EdgeInsets.all(8),
+      child: PlacesList(places: userPlaces)) ,
     )
   ;
   }
