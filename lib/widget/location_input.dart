76ae0b584e839cb62d4888db909a8eb040a90922
Ey-git-hub <eyob.tadesse@astu.edu.et>
2026-08-22 11:18:39 +0300

    added the uri to get the map

--- lib/widget/location_input.dart
+++ lib/widget/location_input.dart
@@ -1,6 +1,8 @@
+import 'package:favorite_places/model/Places.dart';
 import 'package:flutter/material.dart';
 import 'package:location/location.dart';
-
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 class LocationInput extends StatefulWidget{
   const LocationInput({super.key});
 
@@ -38,10 +40,23 @@ if (permissionGranted == PermissionStatus.denied) {
 setState((){
 _isGettingLocation=true;
 });
+// if(${locationData.latitude} == null || ${locationData.longitude} == null){
+  
+//   return;
+// }
 
 locationData = await location.getLocation();
+final url=Uri.parse('https://maps.googleapis.com/maps/api/geocode/json?latlng=${locationData.latitude},${locationData.longitude}&key=AIzaSyBr_8aDyY4YnM9Yr9GgmkUhu5z3VT6ytms');
+final response=await http.get(url);
+final data=json.decode(response.body);
+final address=data['results'][0]['formatted_address'];
 setState((){
-_isGettingLocation=false;
+  _pickedLocation=PlaceLocation(
+    latitude: locationData.latitude,
+    longitude: locationData.longitude,
+    address: address,
+  ) as Location? ;
+  _isGettingLocation=false;
 });
 
   }
