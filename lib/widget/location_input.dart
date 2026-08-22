32f1a4fb4b4a5dc66c20fb726062ad542b9a8007
Ey-git-hub <eyob.tadesse@astu.edu.et>
2026-08-22 10:14:06 +0300

    added loading sppiner for loading location

--- lib/widget/location_input.dart
+++ lib/widget/location_input.dart
@@ -11,6 +11,9 @@ class LocationInput extends StatefulWidget{
   
 }
 class _LocationInput extends State<LocationInput>{
+  Location?  _pickedLocation;
+  var _isGettingLocation=false;
+
   void _getCurrentLocation() async{
     Location location = Location();
 
@@ -25,7 +28,6 @@ if (!serviceEnabled) {
     return;
   }
 }
-
 permissionGranted = await location.hasPermission();
 if (permissionGranted == PermissionStatus.denied) {
   permissionGranted = await location.requestPermission();
@@ -33,11 +35,23 @@ if (permissionGranted == PermissionStatus.denied) {
     return;
   }
 }
+setState((){
+_isGettingLocation=true;
+});
 
 locationData = await location.getLocation();
+setState((){
+_isGettingLocation=false;
+});
+
   }
   @override
   Widget build(BuildContext context) {
+    Widget previewContent=Text(textAlign: TextAlign.center,'No map location added',style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+        color:Theme.of(context).colorScheme.onSurface));
+        if(_isGettingLocation){
+          previewContent=CircularProgressIndicator();
+        }
    return Column(children: [
     Container(
       width: double.infinity,
@@ -46,13 +60,12 @@ locationData = await location.getLocation();
       decoration: BoxDecoration(
         border: Border.all(width: 1, color: Colors.grey),
     ),
-    child: Text(textAlign: TextAlign.center,'No map location added',style: Theme.of(context).textTheme.bodyLarge!.copyWith(
-        color:Theme.of(context).colorScheme.onSurface)),
+    child: previewContent,
     ),
     Row( 
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       children: [
-      TextButton.icon(icon:Icon(Icons.location_on),onPressed: (){}, label: Text('Get Current location')),
+      TextButton.icon(icon:Icon(Icons.location_on),onPressed: _getCurrentLocation, label: Text('Get Current location')),
       TextButton.icon(icon:Icon(Icons.map) ,onPressed:(){} ,label:Text('Select on Map') ,)
     ],)
    ],);
