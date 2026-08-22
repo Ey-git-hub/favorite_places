312740d6555e58350d352f71d59f6af1f7b95f77
Ey-git-hub <eyob.tadesse@astu.edu.et>
2026-08-22 11:18:13 +0300

    added the location property

--- lib/model/Places.dart
+++ lib/model/Places.dart
@@ -1,10 +1,16 @@
 import 'dart:io';
 import 'package:uuid/uuid.dart';
 const uuid=Uuid();
-
+class PlaceLocation{
+  PlaceLocation({required this.latitude, required this.longitude, required this.address});
+  final double latitude;
+  final double longitude;
+  final String address;
+}
 class Places{
-  Places({required this.title, required this.image}): id=uuid.v4();
+  Places({required this.title, required this.image, this.location}): id=uuid.v4();
   final String id;
   final String title;
   final File image;
+  final PlaceLocation? location;
 }
